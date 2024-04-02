a4aa18d00f62503df960424f0a69979bffb153de
ThiwankaReiss <thiwankar2003@gmail.com>
2024-04-02 23:28:31 +0530

    Adding list complete

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
-function addToList(){
-    console.log("h");
+
+let itemsArray=[];
+function addToList() {
+    let itemInput = document.getElementById('list-item-input').value;
+    itemsArray.push(itemInput);
+    console.log(itemInput);
+   
+    let listOfItems=``;
+    itemsArray.forEach(element => {
+        listOfItems += `<li>${element}</li>`;
+    });
+
+    document.getElementById('list-of-items').innerHTML =listOfItems;
 }
